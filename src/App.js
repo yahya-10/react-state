3ecd73edb5dc1fef918b2c066d25ca8732af8bc9
Yahya Akermi <yahyaakermi1992@gmail.com>
2020-10-06 12:44:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+// import './App.css';
+import image from './component/image.jpg'
+import {Switch} from 'antd';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fullName: 'Yahya Akermi' , 
+      bio: 'Be yourself; everyone else is already taken.' , 
+      imgSrc: (image) , 
+      profession: 'web dev'
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Switch />
+        <span>Enable profile display</span>
+        <div>
+            <h1>{this.state.fullName}</h1>
+            <h3>{this.state.profession}</h3>
+            <img src={this.state.imgSrc} alt="" style={{height:"300px", width:"200px"}}/>
+            <h2>{this.state.bio}</h2>
+            </div>
+      </div>
+    );
+  }
 }
 
 export default App;
